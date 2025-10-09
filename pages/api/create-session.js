7ee5e779ed7bf1926af2e233974811298d590299
Lbourgeois98 <73048129+Lbourgeois98@users.noreply.github.com>
2025-10-08 21:11:11 -0600

    Update create-session.js

--- pages/api/create-session.js
+++ pages/api/create-session.js
@@ -1,7 +1,4 @@
 export default async function handler(req, res) {
-  if (req.method !== "POST")
-    return res.status(405).json({ error: "Method not allowed" });
-
   const apiKey = process.env.WERT_API_KEY;
 
   try {
@@ -18,7 +15,6 @@ export default async function handler(req, res) {
         commodity: "USDT",
         network: "ethereum",
         extra: {
-          ...req.body.extra, // keep any other data from frontend
           wallets: [
             {
               name: "USDT",
